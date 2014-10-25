6d0f3f684da763a0e821f5a46bfca9afb26b71c3
Konstantin Tumalevich <userad@gmail.com>
2014-10-26 00:39:23 +0600

    Add path visualizing

--- src/main/java/com/badday/ss/core/utils/SSCommandPathfinder.java
+++ src/main/java/com/badday/ss/core/utils/SSCommandPathfinder.java
@@ -2,9 +2,11 @@ package com.badday.ss.core.utils;
 
 import com.badday.ss.SS;
 
+import net.minecraft.block.Block;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.WorldServer;
@@ -31,14 +33,14 @@ public class SSCommandPathfinder extends CommandBase
         EntityPlayerMP player = (EntityPlayerMP)icommandsender;
         MinecraftServer server = MinecraftServer.getServer();
 
-        if (astring.length == 3)
+        if (astring.length >= 3)
         {
         	if (player.worldObj.provider.dimensionId == SS.instance.spaceDimID) {
-    				long time1 = System.nanoTime();
-    				int x = Integer.parseInt(astring[0]);
-    				int y = Integer.parseInt(astring[1]);
-    				int z = Integer.parseInt(astring[2]);
-    				Pathfinding p = new Pathfinding(player.worldObj, new BlockVec3(player), new BlockVec3(x,y,z));
+            long time1 = System.nanoTime();
+            int x = Integer.parseInt(astring[0]);
+            int y = Integer.parseInt(astring[1]);
+            int z = Integer.parseInt(astring[2]);
+            Pathfinding p = new Pathfinding(player.worldObj, new BlockVec3(player), new BlockVec3(x, y, z));
             long time2 = System.nanoTime();
             while (!p.isDone()) {
               time2 = System.nanoTime();
@@ -49,11 +51,17 @@ public class SSCommandPathfinder extends CommandBase
               }
             }
 
-    				if (SS.Debug) {
-    					System.out.println("[" + SS.MODNAME + "] Pathfinding work to " + x + " " + y + "" + z + " complite. Result: "+p.isDone());
-    					System.out.println("[" + SS.MODNAME + "] Pathfinding work time: "+(time2 - time1) / 1000000.0D + "ms");
-    				}
-    		}
+            System.out.println("[" + SS.MODNAME + "] Pathfinding work to " + x + " " + y + "" + z + " complite. Result: " + p.isDone());
+            System.out.println("[" + SS.MODNAME + "] Pathfinding work time: " + (time2 - time1) / 1000000.0D + "ms");
+
+            if (astring.length == 4)
+              if (p.isDone()) {
+                for (BlockVec3 coord : p.getResult()) {
+                  player.worldObj.setBlock(coord.x, coord.y, coord.z, Blocks.redstone_block);
+                }
+              }
+            }
+
         		//notifyAdmins()
                 //notifyAdmins(icommandsender, "/space: teleporting player " + astring[0] + " to space", new Object[0]);
                 //player = MinecraftServer.getServer().getConfigurationManager().getPlayerForUsername(astring[0]);
